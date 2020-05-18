3fc83c7f28529d4ccb21dd3a9e1aa3612203ced6
paulo <paulo.barcelos@hotmail.com>
2020-05-17 22:36:29 -0300

    service created

---
+++ src/app/shared/oferta.model.ts
@@ -0,0 +1,11 @@
+export class Oferta{
+    public id: number;
+    public categoria: string;
+    public titulo: string;
+    // tslint:disable-next-line: variable-name
+    public descricao_oferta: string;
+    public anunciante: string;
+    public valor: number;
+    public destaque: boolean;
+    public imagens: Array<object>;
+}
